8ec25b118283fef5c07c4836eefbeb823df2b022
RelonZhu <zrl1314@outlook.com>
2019-08-08 23:28:11 +0800

    complete server

--- src/main/java/com/relon/netty/thirdExample/MyChatServerHandler.java
+++ src/main/java/com/relon/netty/thirdExample/MyChatServerHandler.java
@@ -14,7 +14,14 @@ public class MyChatServerHandler extends SimpleChannelInboundHandler<String> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-
+        Channel channel = ctx.channel();
+        channelGroup.forEach(ch -> {
+            if (ch != channel) {
+                ch.writeAndFlush(channel.remoteAddress() + "发送的消息：" + msg + "\n");
+            } else {
+                ch.writeAndFlush("[自己]" + "发送的消息:" + msg + "\n" );
+            }
+        });
     }
 
     /**
@@ -26,11 +33,36 @@ public class MyChatServerHandler extends SimpleChannelInboundHandler<String> {
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
         // 获取当前与服务器端的通道（channel）
         Channel channel = ctx.channel();
-
-        channelGroup.writeAndFlush("[服务器] - " + channel.remoteAddress() + "已加入")；
+        // 对channelGroup中所有的channel进行消息广播
+        channelGroup.writeAndFlush("[服务器] - " + channel.remoteAddress() + "已加入");
 
         // 将每个已经建立连接的channel放入channelGroup
         channelGroup.add(channel);
+    }
+
+    /**
+     * 连接断开时，会调用该方法
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        // 在channelGroup中把当前channel移除
+        // channelGroup.remove(channel); 如果连接断掉，就无须手动移除，会自动从channelgroup中移除
+        channelGroup.writeAndFlush("[服务器] - " + channel.remoteAddress() + "已离开");
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        System.out.println(channel.remoteAddress() + "已上线");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        System.out.println(channel.remoteAddress() + "已下线");
     }
 
     @Override
